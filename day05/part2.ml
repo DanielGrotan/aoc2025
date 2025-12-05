3ed486a59cd43f81c382c301d32759eb7c200b6b
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-12-05 22:09:30 +0100

    solve day05 part2

--- day05/part2.ml
+++ day05/part2.ml
@@ -1,6 +1,33 @@
-let solve _ = 
-  "Part 2"
+let get_ranges ic =
+  let rec loop list =
+    match input_line ic with
+    | "" -> list
+    | line ->
+        let range = String.split_on_char '-' line in
+        let range_start = int_of_string (List.nth range 0) in
+        let range_end = int_of_string (List.nth range 1) in
+        loop ((range_start, range_end) :: list)
+  in
+  loop []
 
-let () =
-  let input = In_channel.(with_open_bin "input.txt" input_all) in
-  Printf.printf "%s\n" (solve input)
+let count_fresh ranges =
+  let total, _ =
+    List.fold_left
+      (fun (acc, max_seen) (start, stop) ->
+        let range_length =
+          if start > max_seen then stop - start + 1
+          else if stop <= max_seen then 0
+          else stop - max_seen
+        in
+        (acc + range_length, max max_seen stop))
+      (0, -1) ranges
+  in
+  total
+
+let solve ic =
+  let ranges = get_ranges ic in
+  let sorted = List.sort compare ranges in
+  let fresh = count_fresh sorted in
+  Printf.printf "Count is: %d\n" fresh
+
+let () = In_channel.with_open_text "input.txt" solve
