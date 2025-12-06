e62229da68023e1b95bbacaf5ce774ab63e56e22
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-12-06 17:46:40 +0100

    setup day06

---
+++ day06-swift/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "day06-swift",
+    targets: [
+        .executableTarget(name: "Part1"),
+        .executableTarget(name: "Part2"),
+    ]
+)
